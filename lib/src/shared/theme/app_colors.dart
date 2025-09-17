80b827578dea76d6663cfbfd893d8a4d2f19d12c
YoussefGhonem1 <youssefghoneem883@gmail.com>
2025-09-17 20:16:02 +0300

    feat:add ui of donate page

--- lib/src/shared/theme/app_colors.dart
+++ lib/src/shared/theme/app_colors.dart
@@ -3,8 +3,21 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color primaryColor = Color(0xFFFE7277);
-  static const Color bgColor = Color(0xFFFFFFFF);
+  static const MaterialColor primaryColor = MaterialColor(
+    0xFFFE7277,
+    <int, Color>{
+       50: Color(0xFFFFEBEC),
+      100: Color(0xFFFFCDCF),
+      200: Color(0xFFFEAAB0),
+      300: Color(0xFFFE8C93),
+      400: Color(0xFFFE7277), 
+      500: Color(0xFFFD5A61),
+      600: Color(0xFFFD4D56),
+      700: Color(0xFFFC3C46),
+      800: Color(0xFFFB2A35),
+      900: Color(0xFFFA0A1A),
+    },
+  );  static const Color bgColor = Color(0xFFFFFFFF);
  static const Color lightGrey = Color(0xFFF2F1F1);
  static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Color(0xFF9E9E9E);
