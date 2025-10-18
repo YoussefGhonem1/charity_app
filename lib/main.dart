5385a4151443a582bacfb09e6d246345a350d143
YoussefGhonem1 <youssefghoneem883@gmail.com>
2025-10-18 15:28:51 +0300

    link app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:charity/src/shared/routing/app_routs.dart';
 import 'package:charity/src/shared/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
